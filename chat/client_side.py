f4d80c5084b5a1f2627a1d27ed5c74a7fbbf5dfe
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-08-22 22:52:03 +0000

    new changes to code base, with socket[depreciated code] socket io refix coming

--- chat/client_side.py
+++ chat/client_side.py
@@ -1,98 +1,63 @@
 #!/usr/bin/python3
-# importing the necessary modules.
+
 import socket
-import sys
-import errno
+import random
+from threading import Thread
+from datetime import datetime
+from colorama import Fore, init, Back
 
-# defining the header length.
-HEADER_LENGTH = 10
+# init colors
+init()
 
-# defining the IP address and Port Number.
-IP = "127.0.0.1"
-PORT = 1234
+# set the available colors
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX,
+    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX,
+    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX,
+    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+]
 
-# Getting the name of the client.
-my_username = input("Username: ")
+# choose a random color for the client
+client_color = random.choice(colors)
 
-"""
-Creating a client socket and providing the address family (socket.AF_INET) and type of connection (socket.SOCK_STREAM), i.e. using TCP connection.
-"""
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server's IP address
+# if the server is not on this machine,
+# put the private (network) IP address (e.g 192.168.1.2)
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5002 # server's port
+separator_token = "<SEP>" # we will use this to separate the client name & message
 
-# Connecting the socket with the IP address and Port Number.
-client_socket.connect((IP, PORT))
+# initialize TCP socket
+s = socket.socket()
+print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+print("[+] Connected.")
+# prompt the client for a name
+name = input("Enter your name: ")
 
-"""
-Setting the connection to a non-blocking state so that the recv() function call will not get blocked. It will return some exceptions only.
-"""
-client_socket.setblocking(False)
+def listen_for_messages():
+    while True:
+        message = s.recv(1024).decode()
+        print("\n" + message)
 
-# Setting the username and header.
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+# make a thread that listens for messages to this client & print them
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
 
-"""
-Here, we have encoded the username into bytes, counted the number of bytes, and then prepared a header of fixed size, that we have encoded to bytes as well.
-"""
-
-# sending the data.
-client_socket.send(username_header + username)
-
-# running an infinite loop to send continuous client requests.
 while True:
+    # input message we want to send to the server
+    to_send =  input()
+    # a way to exit the program
+    if to_send.lower() == 'q':
+        break
+    # add the datetime, name & the color of the sender
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+    # finally, send the message
+    s.send(to_send.encode())
 
-    try:
-        # looping over the received messages and printing them.
-        while True:
-            # getting the header.
-            username_header = client_socket.recv(HEADER_LENGTH)
-
-            # If no header is accepted then finish the connection.
-            if not len(username_header):
-                print('Connection closed by the server')
-                sys.exit()
-
-            # Converting the header to an int value.
-            username_length = int(username_header.decode('utf-8').strip())
-
-            # Decoding the received username.
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            # Decoding the received message.
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            # Printing the message.
-            print(f'{username} > {message}')
-
-            print ("Here 1")
-
-    except IOError as e:
-        # handling the normal error on nonblocking connections.
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
-            sys.exit()
-
-        # If we did not receive anything, then continue.
-        continue
-        print ("Here 2")
-
-    except Exception as e:
-        print("Here 3")
-        print('Reading error: '.format(str(e)))
-        sys.exit()
-    print ("Here 4")
-
-    #getting user message
-    message = input(f'{my_username} > ')
-    #sending the non empty message
-    if message:
-        """
-        Encode the message into bytes, counted the number of bytes, then prepared a header of fixed size, that we have encoded to bytes as well
-        """
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        #sending the message
-        client_socket.send(message_header + message)
-
+# close the socket
+s.close()
--- chat/server_side.py
+++ chat/server_side.py
@@ -1,126 +1,63 @@
 #!/usr/bin/python3
-# importing the necessary modules.
 import socket
-import select
+from threading import Thread
 
-# defining the header length.
-HEADER_LENGTH = 10
+# server's IP address
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5002 # port we want to use
+separator_token = "<SEP>" # we will use this to separate the client name & message
 
-# defining the IP address and Port Number.
-IP = "127.0.0.1"
-PORT = 1234
+# initialize list/set of all connected client's sockets
+client_sockets = set()
+# create a TCP socket
+s = socket.socket()
+# make the port as reusable port
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# bind the socket to the address we specified
+s.bind((SERVER_HOST, SERVER_PORT))
+# listen for upcoming connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-"""
-Creating a server socket and providing the address family (socket.AF_INET) and type of connection (socket.SOCK_STREAM), i.e. using TCP connection.
-"""
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-"""
-Modifying the socket to allow us to reuse the address. We have to provide the socket option level and set the REUSEADDR (as a socket option) to 1 so that address is reused.
-"""
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Binding the socket with the IP address and Port Number.
-server_socket.bind((IP, PORT))
-
-# Making the server listen to new connections.
-server_socket.listen()
-
-# List of sockets for select.select()
-sockets_list = [server_socket]
-
-# A set to contain the connected clients.
-clients = {}
-
-print(f'Listening for connections on IP = {IP} at PORT = {PORT}')
-
-
-# A function for handling the received message.
-def receive_message(client_socket):
-    try:
-        """
-        The received message header contains the message length, its size is defined, and the constant.
-        """
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        """
-        If the received data has no length then it means that the client has closed the connection. Hence, we will return False as no message was received.
-        """
-        if not len(message_header):
-            return False
-
-        # Convert header to an int value
-        message_length = int(message_header.decode('utf-8').strip())
-
-        # Returning an object of the message header and the data of the message data.
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-
-    except:
-        return False
-
-
-# running an infinite loop to accept continuous client requests.
-while True:
-    # Read the data using a select module from the socketLists.
-    read_sockets, _, exception_sockets = select.select(
-        sockets_list, [], sockets_list)
-
-    # Iterating over the notified sockets.
-    for notified_socket in read_sockets:
-        """
-        If the notified socket is a server socket then we have a new connection, so add it using the accept() method.
-        """
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-
-            # Else the client has disconnected before sending his/her name.
-            user = receive_message(client_socket)
-
-            # If False - client disconnected before he sent his name
-            if user is False:
-                continue
-
-            # Adding the accepted socket to select.select() list.
-            sockets_list.append(client_socket)
-
-            # Also adding the username and username header.
-            clients[client_socket] = user
-
-            print('Accepted new connection from {}:{}, username: {}'.format(
-                *client_address, user['data'].decode('utf-8')))
-
-        # Else the existing socket is sending a message so handling the existing client.
+def listen_for_client(cs):
+    """
+    This function keep listening for a message from `cs` socket
+    Whenever a message is received, broadcast it to all other connected clients
+    """
+    while True:
+        try:
+            # keep listening for a message from `cs` socket
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            # client no longer connected
+            # remove it from the set
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
         else:
-            # Receiving the message.
-            message = receive_message(notified_socket)
-
-            # If no message is accepted then finish the connection.
-            if message is False:
-                print('Closed connection from: {}'.format(
-                    clients[notified_socket]['data'].decode('utf-8')))
-
-                # Removing the socket from the list of the socket.socket()
-                sockets_list.remove(notified_socket)
-
-                # Removing the user from the list of users.
-                del clients[notified_socket]
-
-                continue
-
-            # Getting the user by using the notified socket, so that the user can be identified.
-            user = clients[notified_socket]
-
-            print(
-                f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
-
-            # Iterating over the connected clients and broadcasting the message.
-            for client_socket in clients:
-                # Sending to all except the sender.
-                if client_socket != notified_socket:
-                    """
-                    Reusing the message header sent by the sender, and saving the username header sent by the user when he/she connected.
-                    """
-                    client_socket.send(
-                        user['header'] + user['data'] + message['header'] + message['data'])
+            # if we received a message, replace the <SEP>
+            # token with ": " for nice printing
+            msg = msg.replace(separator_token, ": ")
+        # iterate over all connected sockets
+        for client_socket in client_sockets:
+            # and send the message
+            client_socket.send(msg.encode())
 
 
+while True:
+    # we keep listening for new connections all the time
+    client_socket, client_address = s.accept()
+    print(f"[+] {client_address} connected.")
+    # add the new connected client to connected sockets
+    client_sockets.add(client_socket)
+    # start a new thread that listens for each client's messages
+    t = Thread(target=listen_for_client, args=(client_socket,))
+    # make the thread daemon so it ends whenever the main thread ends
+    t.daemon = True
+    # start the thread
+    t.start()
+
+# close client sockets
+for cs in client_sockets:
+    cs.close()
+# close server socket
+s.close()
