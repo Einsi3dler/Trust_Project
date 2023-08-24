cfe4ec321e652b141a5cfc7f3173f41108c3131e
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-08-24 12:01:50 -0400

    Update Api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,7 @@ from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/vi")
 
 
+from api.v1.views.buyers import *
+from api.v1.views.sellers import *
+from api.v1.views.user import *
+from api.v1.views.transactions import *
