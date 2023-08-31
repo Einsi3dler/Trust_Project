c419f4c05e269c59bd9c0829b498b15fdbae7d28
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-08-30 21:05:30 -0400

    Frontend

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,25 +1,37 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Offcanvas } from 'react-bootstrap';
 import './header.css';
 
 export default function PageHeader () {
 	return (
-		<Navbar  className="bg-primary">
+		<Navbar fixed="top" data-bs-theme expand="lg" collapseOnSelect  className="bg-primary header">
 			<Container fluid>
 			<Navbar.Brand className='logo'>TRUST</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-        	<Navbar.Collapse id="basic-navbar-nav">
-         	 <Nav className="nav-menu d-none d-lg-flex">
+			<Navbar.Offcanvas
+			 id="basic-navbar-nav"
+			 aria-labelledby="navbar-label"
+			 placement="end"
+			 className="bg-dark"
+			 >
+			<Offcanvas.Header closeButton>
+            <Offcanvas.Title id="navbar-label">
+                  Trust
+            </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+         	 <Nav className="nav-menu justify-content-end flex-grow-1 pe-3">
 			<Nav.Link>Home</Nav.Link>
 			<Nav.Link>Features</Nav.Link>
 			<Nav.Link>Contact</Nav.Link>
 			<Nav.Link>About</Nav.Link>
 			<Nav.Link>Services</Nav.Link>
 			</Nav>
-			<Nav className="nav-user d-none d-lg-flex">
+			<Nav className="nav-menu justify-content-end flex-grow-1 pe-3">
 			<Nav.Link href="/sign-in">Sign In </Nav.Link>
 			<Nav.Link href="/sign-up">Sign Up </Nav.Link>
 			</Nav>
-			</Navbar.Collapse>
+			</Offcanvas.Body>
+			</Navbar.Offcanvas>
 			</Container>
 		</Navbar>
 	)
