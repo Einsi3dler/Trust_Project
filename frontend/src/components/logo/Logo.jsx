127a637af2d881dd50b3937e34dc5d197c809a5b
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-09-21 19:11:35 -0400

    Presentation completed

--- frontend/src/components/logo/Logo.jsx
+++ frontend/src/components/logo/Logo.jsx
@@ -2,20 +2,13 @@ import PropTypes from "prop-types";
 import { forwardRef } from "react";
 import { Link as RouterLink } from "react-router-dom";
 // @mui
-import { useTheme } from "@mui/material/styles";
 import { Box, Link } from "@mui/material";
 
 // ----------------------------------------------------------------------
 
 // eslint-disable-next-line react/display-name
 const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
-  const theme = useTheme();
 
-  const PRIMARY_LIGHT = theme.palette.primary.light;
-
-  const PRIMARY_MAIN = theme.palette.primary.main;
-
-  const PRIMARY_DARK = theme.palette.primary.dark;
 
 
 
